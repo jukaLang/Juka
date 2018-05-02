cf7415ec71be6c738c8fdc7c1ebcde9078abd92a
TheAndreiM <andreifundrei@gmail.com>
2018-05-02 19:58:08 -0400

    Scintilla

--- src/DreamCompiler/grammar/DreamGrammarLexer.cs
+++ src/DreamCompiler/grammar/DreamGrammarLexer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// Generated from C:\Users\Andrey\Desktop\DReAM\src\\DreamCompiler\grammar\DreamGrammar.g4 by ANTLR 4.7.1
+// Generated from C:\Users\AndreiM\Desktop\DReAM\src\\DreamCompiler\grammar\DreamGrammar.g4 by ANTLR 4.7.1
 
 // Unreachable code detected
 #pragma warning disable 0162
