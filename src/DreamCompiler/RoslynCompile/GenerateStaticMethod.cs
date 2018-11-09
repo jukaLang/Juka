b27b30aff753b7b644939a052a8cf4177e085612
Don <don.reamey@outlook.com>
2018-11-08 16:21:13 -0800

    Adding code to generate a main method

---
+++ src/DreamCompiler/RoslynCompile/GenerateStaticMethod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DReAMCompiler.RoslynCompile
+{
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis;
+
+    class GenerateStaticMethod
+    {
+        public MethodDeclarationSyntax CreateClass(String methodName)
+        {
+            IdentifierNameSyntax name = SyntaxFactory.IdentifierName(methodName);
+            TypeDeclarationSyntax returnType = SyntaxFactory.TypeDeclaration(SyntaxKind.VoidKeyword, "void");
+            //SyntaxFactory.MethodDeclaration(TypeSyntax)
+            return null;
+        }
+    }
+}
