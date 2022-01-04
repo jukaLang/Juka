6395a4219a55f24f19044cc02783123414e71cc7
AndreiM <andreim@jukalang.com>
2022-01-04 00:23:20 -0500

    Initial JukaNetwork Concept

---
+++ src/JukaNetwork/Block.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+//Based on Henry He code
+
+namespace JukaNetwork
+{
+    internal class Block
+    {
+        public int Nonce { get; set; } = 0;
+        public int Index { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string PreviousHash { get; set; }
+        public string Hash { get; set; }
+        public string Data { get; set; }
+
+        public Block(DateTime timeStamp, string previousHash, string data)
+        {
+            Index = 0;
+            TimeStamp = timeStamp;
+            PreviousHash = previousHash;
+            Data = data;
+            Hash = CalculateHash();
+        }
+
+        public string CalculateHash()
+        {
+            SHA256 sha256 = SHA256.Create();
+
+            byte[] inputBytes = Encoding.ASCII.GetBytes($"{TimeStamp}-{PreviousHash ?? ""}-{Data}-{Nonce}");
+            byte[] outputBytes = sha256.ComputeHash(inputBytes);
+
+            return Convert.ToBase64String(outputBytes);
+        }
+
+        public void Mine(int difficulty)
+        {
+            var leadingZeros = new string('7', difficulty);
+            while (this.Hash == null || this.Hash.Substring(0, difficulty) != leadingZeros)
+            {
+                this.Nonce++;
+                this.Hash = this.CalculateHash();
+            }
+        }
+    }
+}
