1e74c6208ebb98050cccb87cb164245a8716aae1
AndreiM <andreim@jukalang.com>
2022-05-16 17:52:14 -0400

    Extension Update

--- src/JukaUnitTest/CompilerUnitTest.cs
+++ src/JukaUnitTest/CompilerUnitTest.cs
@@ -11,11 +11,11 @@ namespace JukaUnitTest
         [TestMethod]
         public void TestSourceAsFile()
         {
-            const string returnValue = "0.769\r\nSTART\r\nfoobar-32external\r\nEND\r\n0.77\r\n";
+            const string returnValue = "AsdfA";
 
             Compiler compiler = new Compiler();
 
-            var outputValue = compiler.Go(@"..\..\..\..\..\examples\test.jlr");
+            var outputValue = compiler.Go(@"..\..\..\..\..\examples\test.juka");
             if (compiler.HasErrors())
             {
                 var errors = compiler.ListErrors();
@@ -25,37 +25,21 @@ namespace JukaUnitTest
                 }
             }
 
-            Assert.AreEqual(outputValue, returnValue);
+            Assert.AreEqual(returnValue, outputValue);
         }
 
         [TestMethod]
         public void TestSourceAsFile2()
         {
-            // NOT WORKING CURRENTLY
             Compiler compiler = new Compiler();
-            var outputValue = compiler.Go(@"..\..\..\..\..\examples\test2.jlr");
+            var outputValue = compiler.Go(@"..\..\..\..\..\examples\test2.juka");
             if (compiler.HasErrors())
             {
                 throw new Exception("Parser exceptions:\r\n" + String.Join("\r\n", compiler.ListErrors()));
             }
-            //Assert.AreEqual(outputValue, "foo\r\n");
+            Assert.AreEqual("foo", outputValue);
         }
 
-
-        [TestMethod]
-        public void TestSourceAsString()
-        {
-            string t = new string("abc");
-            Compiler compiler = new Compiler();
-            string sourceAsString = "func x() = { printLine(\"AsdfA\");} x();";
-
-            var outputValue = compiler.Go(sourceAsString, false);
-            if (compiler.HasErrors())
-            {
-                throw new Exception("Parser exceptions:\r\n" + String.Join("\r\n", compiler.ListErrors()));
-            }
-            Assert.AreEqual(outputValue, "AsdfA\r\n");
-        }
 
         [TestMethod]
         public void TestOutput()
@@ -68,8 +52,7 @@ namespace JukaUnitTest
             {
                 throw new Exception("Parser exceptions:\r\n" + String.Join("\r\n", compiler.ListErrors()));
             }
-            // TODO Main does not run
-            //Assert.AreEqual(outputValue, "AsdfA\r\n");
+            Assert.AreEqual("AsdfA", outputValue);
         }
 
         [TestMethod]
@@ -83,14 +66,13 @@ namespace JukaUnitTest
             {
                 throw new Exception("Parser exceptions:\r\n" + String.Join("\r\n", compiler.ListErrors()));
             }
-            Assert.AreEqual(outputValue, "");
+            Assert.AreEqual("", outputValue);
         }
 
         [TestMethod]
         public void TestEmptyComment()
         {
             Compiler compiler = new Compiler();
-            ///*saddsadsa*dasdas\\*/asdasd*/ /*  */ //!sssd
             string sourceAsString = "/*saddsadsa*dasdas\\*/asdasd*//**///!sssd";
 
             var outputValue = compiler.Go(sourceAsString, false);
@@ -98,7 +80,7 @@ namespace JukaUnitTest
             {
                 throw new Exception("Parser exceptions:\r\n" + String.Join("\r\n", compiler.ListErrors()));
             }
-            Assert.AreEqual(outputValue, "");
+            Assert.AreEqual("", outputValue);
         }
 
 
