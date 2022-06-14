c4a71634ce95464859e7fc6819521b682c4fcedd
AndreiM <andreim@jukalang.com>
2022-06-14 02:10:23 -0400

    Update Program.cs

--- src/JukaApi/Program.cs
+++ src/JukaApi/Program.cs
@@ -3,19 +3,6 @@ using System.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-                      policy =>
-                      {
-                          policy.WithOrigins("http://localhost:3000",
-                                              "https://localhost:3000",
-                                              "https://jukalang.com/");
-                      });
-});
-
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -31,7 +18,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseCors(MyAllowSpecificOrigins);
+app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials()); // allow credentials
 
 
 
