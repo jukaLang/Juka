5fe957bd60ca3369635570fb76d35b65118c8640
AndreiM <andreim@jukalang.com>
2022-07-02 23:34:42 -0400

    Removed XConsole (Not Needed Anymore to keep OSes consistent)

--- src/Juka/Program.cs
+++ src/Juka/Program.cs
@@ -1,62 +1,37 @@
-﻿using Juka;
-using System.Runtime.InteropServices;
-
-string? sourceAsString;
+﻿string? sourceAsString;
 string? readline;
 
 if (args == null || args.Length == 0)
 {
     Console.Title = "Juka Compiler";
     Console.WriteLine("♥ Welcome to Juka Compiler. If you need to run a file, pass it as an argument ♥");
-    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-    {
-        Console.WriteLine("(Press F10 to run the code)");
-    }
-    else
-    {
-        Console.WriteLine("Press Return 3 times to run the code");
-    }
+    Console.WriteLine("Press Return 3 times to run the code");
+
     while (true)
     {
         Console.Write("→ ");
         sourceAsString = "";
 
-
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        int counter = 0;
+        while (true)
         {
-            while (true)
+            readline = Console.ReadLine();
+            if (readline == "")
             {
-                readline = XConsole.CancelableReadLine(out bool isCancelled);
-
-                sourceAsString += readline;
-                if (isCancelled)
+                counter++;
+                if (counter == 3)
                 {
                     break;
                 }
             }
-        }
-        else
-        {
-            int counter = 0;
-            while (true)
+            else
             {
-                readline = Console.ReadLine();
-                if (readline == "")
-                {
-                    counter++;
-                    if (counter == 3)
-                    {
-                        break;
-                    }
-                }
-                else
-                {
-                    counter = 0;
-                }
-                sourceAsString += readline;
-
+                counter = 0;
             }
+            sourceAsString += readline;
+
         }
+
         Console.WriteLine(Environment.NewLine + "=====OUTPUT:=======");
         Console.WriteLine(new JukaCompiler.Compiler().Go(sourceAsString, false));
     }
