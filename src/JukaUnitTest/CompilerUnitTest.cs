ad84eecebb5fee2750e87d51c38369fad4533638
Don Reamey <doreamey@microsoft.com>
2022-07-29 22:56:42 -0700

    Clean up

--- src/JukaUnitTest/CompilerUnitTest.cs
+++ src/JukaUnitTest/CompilerUnitTest.cs
@@ -12,9 +12,9 @@ namespace JukaUnitTest
         public void TestEmptyString()
         {
             Compiler compiler = new Compiler();
-            string sourceAsString = "   ";
+            string sourceAsString = @"../../../../../examples/dontest.juk";
 
-            var outputValue = compiler.Go(sourceAsString, false);
+            var outputValue = compiler.Go(sourceAsString, true);
             if (compiler.HasErrors())
             {
                 throw new Exception("Parser exceptions:\r\n" + String.Join("\r\n", compiler.ListErrors()));
@@ -34,9 +34,6 @@ namespace JukaUnitTest
                 throw new Exception("Parser exceptions:\r\n" + String.Join("\r\n", compiler.ListErrors()));
             }
             Assert.AreEqual("", outputValue);
-        }
-
-             Assert.AreEqual("AsdfA" + Environment.NewLine, outputValue);
         }
 
         [TestMethod]
@@ -182,99 +179,5 @@ namespace JukaUnitTest
             }
             //Assert.AreEqual("Hello World", outputValue);
         }
-
-
-
-
-
-
-        /*[TestMethod]
-        public void TestEmptyFunc()
-        {
-            try
-            {
-                Compiler compiler = new Compiler();
-                compiler.Go("", "function main() =  {}");
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-
-        [TestMethod]
-        public void TestBrokenToken()
-        {
-            try
-            {
-                Compiler compiler = new Compiler();
-                compiler.Go("", "asdfunct main() =  {");
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-
-        [TestMethod]
-        public void TestEmptyFunc()
-        {
-            var mockScanner = new Mock<IScanner>();
-            var tokenArray = StringToToken("func(){}");
-
-            int bufferCount = -1;
-
-            mockScanner.Setup(f => f.ReadToken() )
-                .Returns(()=>
-            {
-                bufferCount++;
-
-                if (bufferCount == tokenArray.Length)
-                {
-                    return new Token(TokenType.Eof);
-                }
-
-                return tokenArray[bufferCount];
-            });
-
-            mockScanner.Setup(f => f.PutTokenBack()).Callback(() => 
-            {
-                bufferCount--;
-            });
-
-            try
-            {
-                int sequenceCount = 5;
-
-                ILexicalAnalysis lexical = new LexicalAnalysis();
-                var llm = lexical.Analyze(mockScanner.Object);
-
-                Assert.IsNotNull(llm);
-                Assert.AreEqual(sequenceCount, llm.Count, $"Lexem count is off. Ensure the Token Array is accurate.");
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-        
-
-        private Token[] StringToToken(String tokenString)
-        {
-            var tokenArray = new Token[tokenString.Length];
-            for (int counter = 0; counter < tokenString.Length; counter++)
-            {
-                if (Char.IsLetter(tokenString[counter]))
-                {
-                    tokenArray[counter] = new Token(TokenType.Character, tokenString[counter]);
-                }
-                else
-                {
-                    tokenArray[counter] = new Token(TokenType.Symbol, tokenString[counter]);
-                }
-            }
-            return tokenArray;
-        }*/
-
     }
 }
