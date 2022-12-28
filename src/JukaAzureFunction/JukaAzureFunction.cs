822ed5a2c2f1693b494496f0ec7c1f3f001cb7e9
AndreiM <andreim@jukalang.com>
2022-12-28 00:21:35 -0500

    Azure Function Update
    Internal Changes:
    - Azure Functions should now run correctly on Microsoft Azure

--- src/JukaAzureFunction/JukaAzureFunction.cs
+++ src/JukaAzureFunction/JukaAzureFunction.cs
@@ -7,6 +7,10 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Reflection;
+using AzureFunctions.Extensions.Swashbuckle.Attribute;
+using AzureFunctions.Extensions.Swashbuckle;
+using System.Net.Http;
 
 namespace JukaAzureFunction
 {
@@ -22,9 +26,13 @@ namespace JukaAzureFunction
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             code ??= data?.code;
 
-            if (code == "")
+            string assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "DEBUG";
+            if (assemblyVersion == "0.0.0.1") assemblyVersion = "DEBUG";
+          
+
+            if (code == null || code == "")
             {
-                return new OkObjectResult(new { output = "\"Welcome to Juka Azure Function version \"+ assemblyVersion + \"! To execute a program, send a GET or a POST request to \\\"/code_you_want_to_execute\\\". You can also send a POST request to '/' to execute code embedded in body (raw).\");\r\n\r\n" });
+                return new OkObjectResult(new { output = "\"Welcome to Juka Azure Function version \"+ assemblyVersion + \"! To execute a program, send a GET or a POST request to \\\"/api/JukaAzureFunction/code=func main()={}\\\" \");\r\n\r\n" });
             }
 
             log.LogInformation("Running code: " + code);
@@ -42,114 +50,23 @@ namespace JukaAzureFunction
         }
     }
 
-
-        /*public static class JukaAzureFunction
-        FUTRURE USE FOR COMPILER
+    public static class SwaggerFunctions
+    {
+        [SwaggerIgnore]
+        [FunctionName("Swagger")]
+        public static Task<HttpResponseMessage> Swagger(
+                [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "swagger/json")] HttpRequestMessage req,
+                [SwashBuckleClient] ISwashBuckleClient swasBuckleClient)
         {
-            [FunctionName("JukaAzureFunction")]
-            public static async Task<IActionResult> Run(
-                [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
-            {
-                log.LogInformation("Running code");
-
-                string code = "";
-                code = req.Query["code"];
-
-                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-                dynamic data = JsonConvert.DeserializeObject(requestBody);
-                code = code ?? data?.code;
-
-                JukaCompiler.Compiler compiler = new JukaCompiler.Compiler();
-                string sourceAsString = "func x() = { printLine(\"AsdfA\");} x();";
-                compiler.Go(sourceAsString, false);
-
-                return new OkObjectResult(compiler.HasErrors());
-                //return Roslyn(code, responseMessage);
-            }
-
-            private static IActionResult Roslyn(string code, string responseMessage)
-            {
-                //Based on Josh Varty and Damir code
-                var tree = CSharpSyntaxTree.ParseText(@"
-                using System;
-
-                namespace ConsoleApp1
-                {
-                    public class Program
-                    {
-                        static void Main(string[] args)
-                        {
-                            Console.WriteLine(""" + code + @""");
-                            //Console.ReadLine();
-                        }
-                    }
-                }
-                ");
-
-                var assemblyPath = Path.ChangeExtension("output", "exe");
-
-                File.WriteAllText(
-                    Path.ChangeExtension(assemblyPath, "runtimeconfig.json"),
-                    GenerateRuntimeConfig()
-                );
-
-                var dotNetCoreDir = Path.GetDirectoryName(typeof(object).GetTypeInfo().Assembly.Location);
-                var mscorlib = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
-                var console = MetadataReference.CreateFromFile(typeof(Console).GetTypeInfo().Assembly.Location);
-                var myruntime = MetadataReference.CreateFromFile(Path.Combine(dotNetCoreDir, "System.Runtime.dll"));
-
-                //We first have to choose what kind of output we're creating: DLL, .exe etc.
-                var options = new CSharpCompilationOptions(OutputKind.ConsoleApplication);
-                options = options.WithAllowUnsafe(true);                                //Allow unsafe code;
-                options = options.WithOptimizationLevel(OptimizationLevel.Debug);     //Set optimization level
-                options = options.WithPlatform(Platform.X64);                           //Set platform
-
-                var compilation = CSharpCompilation.Create("MyCompilation",
-                    syntaxTrees: new[] { tree },
-                    references: new[] { mscorlib, console, myruntime },
-                    options: options);
-
-                //Emitting to file is available through an extension method in the Microsoft.CodeAnalysis namespace
-                var emitResult = compilation.Emit(assemblyPath);
-
-                Process process = new Process();
-                process.StartInfo.FileName = "dotnet";
-                process.StartInfo.Arguments = assemblyPath; // Note the /c command (*)
-                //process.StartInfo.UseShellExecute = false;
-                process.StartInfo.RedirectStandardOutput = true;
-                process.StartInfo.RedirectStandardError = true;
-                process.Start();
-                //* Read the output (or the error)
-                string output = process.StandardOutput.ReadToEnd();
-                //Console.WriteLine(output);
-                string err = process.StandardError.ReadToEnd();
-                //Console.WriteLine(err);
-                process.WaitForExit();
-
-                return new OkObjectResult(output + err);
-            }
-
-            private static string GenerateRuntimeConfig()
-            {
-                using (var stream = new MemoryStream())
-                {
-                    using (var writer = new Utf8JsonWriter(
-                        stream,
-                        new JsonWriterOptions() { Indented = true }
-                    ))
-                    {
-                        writer.WriteStartObject();
-                        writer.WriteStartObject("runtimeOptions");
-                        writer.WriteStartObject("framework");
-                        writer.WriteString("name", "Microsoft.NETCore.App");
-                        writer.WriteString("version",Environment.Version.ToString());
-                        writer.WriteEndObject();
-                        writer.WriteEndObject();
-                        writer.WriteEndObject();
-                    }
-
-                    return Encoding.UTF8.GetString(stream.ToArray());
-                }
-            }
-        }*/
+            return Task.FromResult(swasBuckleClient.CreateSwaggerJsonDocumentResponse(req));
+        }
+        [SwaggerIgnore]
+        [FunctionName("SwaggerUI")]
+        public static Task<HttpResponseMessage> SwaggerUI(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "swagger/ui")] HttpRequestMessage req,
+        [SwashBuckleClient] ISwashBuckleClient swasBuckleClient)
+        {
+            return Task.FromResult(swasBuckleClient.CreateSwaggerUIResponse(req, "swagger/json"));
+        }
     }
+}
