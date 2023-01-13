603da9c40a07fce606c173fb9afbdba6aa5b7c3f
AndreiM <andreim@jukalang.com>
2023-01-12 23:06:48 -0500

    Azure Function Code Update

--- src/JukaAzureFunction/JukaAzureFunction.cs
+++ src/JukaAzureFunction/JukaAzureFunction.cs
@@ -1,15 +1,15 @@
 using System.Net;
 using System.Reflection;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http;
+using AzureFunctions.Extensions.Swashbuckle.Attribute;
+using AzureFunctions.Extensions.Swashbuckle;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
-
 
 namespace JukaAzureFunction
 {
@@ -23,32 +23,42 @@ namespace JukaAzureFunction
         }
 
 
-        [FunctionName("JukaAzureFunction")]
+        [SwaggerIgnore]
+        [FunctionName("SwaggerUi")]
+        public static Task<HttpResponseMessage> Run2(
+            [Microsoft.Azure.Functions.Worker.HttpTrigger(AuthorizationLevel.Function, "get", Route = "Swagger/ui")] HttpRequestMessage req,
+            [SwashBuckleClient] ISwashBuckleClient swashBuckleClient)
+        {
+            return Task.FromResult(swashBuckleClient.CreateSwaggerUIResponse(req, "swagger/json"));
+        }
+
+
+        [Function("JukaAzureFunction")]
         [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "Pass Code as Get or Post", In = OpenApiSecurityLocationType.Query)]
         [OpenApiParameter(name: "code", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Code** parameter")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
-        public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
+
+        public HttpResponseData Run([Microsoft.Azure.Functions.Worker.HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("JukaAzureFunction trigger is running.");
 
-            string code = req.Query["code"];
+            var query = System.Web.HttpUtility.ParseQueryString(req.Url.Query);
+            var code = query["code"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            code ??= data?.code;
 
             string assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "DEBUG";
             if (assemblyVersion == "0.0.0.1")
             {
                 assemblyVersion = "DEBUG";
             }
-          
 
             if (string.IsNullOrEmpty(code))
             {
-                return new OkObjectResult(new { output = "\"Welcome to Juka Azure Function version "+ assemblyVersion + "! To execute a program, send a GET or a POST request to \\\"/api/JukaAzureFunction/code=func main()={}\\\" \");\r\n\r\n" });
+                var response = req.CreateResponse(HttpStatusCode.OK);
+                response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+                response.WriteString("Welcome to Juka Azure Function version \""+ assemblyVersion + "\"! To execute a program, send a GET or a POST request to \"/api/JukaAzureFunction/?code=func main()={}\"");
+                return response;
             }
 
             _logger.LogInformation("Running code: " + code);
@@ -59,10 +69,18 @@ namespace JukaAzureFunction
 
             if (compiler.HasErrors())
             {
+
+                var response2 = req.CreateResponse(HttpStatusCode.OK);
+                response2.Headers.Add("Content-Type", "text/plain; charset=utf-8");
                 var errors = string.Join(Environment.NewLine, compiler.ListErrors());
-                return new OkObjectResult(new { errors, original = code });
+                response2.WriteString(JsonConvert.SerializeObject(new { errors, original = code }));
+                return response2;
             }
-            return new OkObjectResult(new { output = outputValue, original = code });
+
+            var response3 = req.CreateResponse(HttpStatusCode.OK);
+            response3.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            response3.WriteString(JsonConvert.SerializeObject(new { output = outputValue, original = code }));
+            return response3;
         }
     }
 }
