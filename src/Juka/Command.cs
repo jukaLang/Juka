aa0f5510a0d268ffc70b670dfc1708e0ef149cfa
AndreiM <andreim@jukalang.com>
2023-01-24 14:49:40 -0500

    REPL now displays when code is running
    Visible Changes to User:
    - Displays when REPL is running
    
    Internal:
    - Work on refactoring commands

---
+++ src/Juka/Command.cs
@@ -0,0 +1,11 @@
+﻿namespace Juka
+{
+    public class Command
+    {
+        int id;
+        string shortName;
+        string longName;
+        string description;
+        delegate double MyFunction(double x);
+    }
+}
